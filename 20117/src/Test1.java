206c9e36148ff83588f6777753839fc58d9a1058
user <user@edi17>
2017-04-14 14:44:57 +0900

    a

--- 20117/src/Test1.java
+++ 20117/src/Test1.java
@@ -2,10 +2,8 @@
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("human");
-		System.out.println("people");
-		System.out.println("pal");
-		System.out.println("nam");
+		System.out.println("a");
+		
 	}
 
 }
